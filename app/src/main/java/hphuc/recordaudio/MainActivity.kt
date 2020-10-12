19643316baeca800b2705a044996f56ebc95fe42
phuclh@minerva.vn <hoangphuc98>
2020-10-12 17:23:23 +0700

    - first commit

--- app/src/main/java/hphuc/recordaudio/MainActivity.kt
+++ app/src/main/java/hphuc/recordaudio/MainActivity.kt
@@ -1,11 +1,31 @@
 package hphuc.recordaudio
 
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnStart : Button
+    private val FILE_PATH = Environment.getExternalStorageDirectory().absolutePath
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnStart = findViewById(R.id.btnStart)
+
+        val mediaRecord = MediaRecorder()
+        mediaRecord.setAudioSource(MediaRecorder.AudioSource.MIC)
+        mediaRecord.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+        mediaRecord.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+        mediaRecord.setOutputFile("${FILE_PATH}/test.3gp")
+        mediaRecord.prepare()
+
+        btnStart.setOnClickListener {
+            mediaRecord.start()
+        }
     }
 }
